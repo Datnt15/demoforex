26b50cb66ab4bf5a50a94589bdc3cabc614db18b
Datnt15 <tiendatbt19@gmail.com>
2016-10-19 08:46:32 +0700

    Tạo trang miniadmin

--- admin.php
+++ admin.php
@@ -9,15 +9,15 @@
  Dual licensed: GPL v2 and MIT, see texts at http://opensource.org/licenses/
 */
 
- $ACCESS_PWD=''; #!!!IMPORTANT!!! this is script access password, SET IT if you want to protect you DB from public access
+ $ACCESS_PWD='thongkhanh'; #!!!IMPORTANT!!! this is script access password, SET IT if you want to protect you DB from public access
 
  #DEFAULT db connection settings
  # --- WARNING! --- if you set defaults - it's recommended to set $ACCESS_PWD to protect your db!
  $DBDEF=array(
- 'user'=>"",#required
- 'pwd'=>"", #required
- 'db'=>"",  #optional, default DB
- 'host'=>"",#optional
+ 'host'=>"localhost",#optional
+ 'user'=>"forex_user",#required
+ 'pwd'=>"forex_user123!@#", #required
+ 'db'=>"dh_forex",  #optional, default DB
  'port'=>"",#optional
  'chset'=>"utf8",#optional, default charset
  );
